032fa17558730c5d033f906504489d0f47eefd31
Steve Sperandeo <steve.sperandeo@gmail.com>
2011-02-21 13:47:33 -0800

    Added numerical validation class.

---
+++ source/php/Validation/Numerics.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+* This file is part of the Altumo library.
+* 
+* (c) Steve Sperandeo <steve.sperandeo@altumo.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+
+
+
+namespace Altumo\Validation;
+ 
+ 
+/**
+* This class contains functions for number validation.
+* These functions will return the sanitized data too.
+* 
+* @author Steve Sperandeo <steve.sperandeo@altumo.com>
+*/
+class Numerics{
+    
+    
+    /**
+    * Throws an exception if this is not an unsigned integer.
+    * If this is a string, recasts as integer and returns it.
+    * 
+    * @param mixed $integer
+    * @throws Exception if argument passed is not an integer or can't be cast as an integer.
+    * @return integer
+    */
+    static public function assertUnsignedInteger( $integer ){
+        
+        $integer = self::assertInteger( $integer );
+        if( $integer < 0 ){
+            throw new \Exception( 'Integer cannot be negative.' );
+        }
+        return $integer;
+        
+    }
+    
+    
+    
+    
+    /**
+    * Throws an exception if this is not a positive integer.
+    * If this is a string, recasts as integer and returns it.
+    * 
+    * @param mixed $integer
+    * @throws Exception if argument passed is not an integer or can't be cast as an integer.
+    * @return integer
+    */
+    static public function assertPositiveInteger( $integer ){
+        
+        $integer = self::assertInteger( $integer );
+        if( $integer <= 0 ){
+            throw new \Exception( 'Integer cannot be negative or zero.' );
+        }
+        return $integer;
+        
+    }
+    
+    
+    
+    /**
+    * Throws an exception if this is not an integer.
+    * If this is a string, recasts as integer and returns it.
+    * 
+    * @param mixed $integer
+    * @throws Exception if argument passed is not an integer or can't be cast as an integer.
+    * @return integer
+    */
+    static public function assertInteger( $integer ){
+        
+        if( is_integer($integer) ){
+            return $integer;
+        }
+        if( !is_numeric($integer) || floor($integer) != $integer ){
+            throw new \Exception('Value passed is not an integer.');
+        }
+        return (integer)$integer;
+        
+    }
+    
+    
+    
+   
+    
+    /**
+    * Throws an exception if this is not an unsigned double.
+    * If this is a string, recasts as double and returns it.
+    * 
+    * @param mixed $double
+    * @throws Exception if argument passed is not a double or can't be cast as a double.
+    * @return double
+    */
+    static public function assertUnsignedDouble( $double ){
+        
+        $double = self::assertDouble( $double );
+        if( $double < 0.0 ){
+            throw new \Exception( 'Double cannot be negative.' );
+        }
+        return $double;
+        
+    }
+    
+    
+    
+    
+    /**
+    * Throws an exception if this is not a positive double.
+    * If this is a string, recasts as double and returns it.
+    * 
+    * @param mixed $double
+    * @throws Exception if argument passed is not a double or can't be cast as a double.
+    * @return double
+    */
+    static public function assertPositiveDouble( $double ){
+        
+        $double = self::assertDouble( $double );
+        if( $double <= 0.0 ){
+            throw new \Exception( 'Double cannot be negative or zero.' );
+        }
+        return $double;
+        
+    }
+    
+    
+    
+    /**
+    * Throws an exception if this is not an integer.
+    * If this is a string, recasts as double and returns it.
+    * 
+    * @param mixed $double
+    * @throws Exception if argument passed is not a double or can't be cast as a double.
+    * @return double
+    */
+    static public function assertDouble( $double ){
+        
+        if( is_double($double) ){
+            return $double;
+        }
+        if( !is_numeric($double) ){
+            throw new \Exception('Value passed is not a double.');
+        }
+        return (double)$double;
+        
+    }
+    
+    
+    
+}
