d97c573bca2a0436a27055e6f44cb43a6b2b534d
Steve Sperandeo <steve.sperandeo@gmail.com>
2011-04-13 19:51:41 -0700

    Update XmlElement documentation and unit test.

--- source/php/Xml/XmlElement.md
+++ source/php/Xml/XmlElement.md
@@ -8,10 +8,41 @@ more consistent interface. It throws exceptions instead of relying on errors.
 Sample Usage
 ------------
 
-    $xml_file = new \Altumo\Xml\XmlElement();
-    $xml_file->setContentsByString(
+Set contents by string:
+
+    $xml_element = new \Altumo\Xml\XmlElement();
+    $xml_element->setContentsByString(
         '<?xml version="1.0" encoding="UTF-8" ?>' . 
         '<hello>World</hello>'
     );
 
-    $this->assertTrue( $xml_file->xpath('.') === 'World' );
+    $this->assertTrue( $xml_element->xpath('.') === 'World' );
+    
+
+Read from a file:
+    
+    $xml_element = new \Altumo\Xml\XmlElement();
+    $xml_element->loadFromFile( __DIR__ . '/test_data/minimal.xml' );
+    var_dump( $xml_element->queryWithXpath('food/sodium', \Altumo\Xml\XmlElement::RETURN_TYPE_STRING) );
+    
+    array(2) {
+      [0]=>
+      string(3) "210"
+      [1]=>
+      string(3) "510"
+    }
+    
+
+Read from a file and pass the contents to the constructor as a string:
+    
+    $file_contents = file_get_contents( __DIR__ . '/test_data/minimal.xml' );
+    $xml_element = new \Altumo\Xml\XmlElement( $file_contents );
+    var_dump( $xml_element->queryWithXpath('food/sodium', \Altumo\Xml\XmlElement::RETURN_TYPE_STRING) );
+    
+    array(2) {
+      [0]=>
+      string(3) "210"
+      [1]=>
+      string(3) "510"
+    }
+    
--- source/php/Xml/XmlElementTest.php
+++ source/php/Xml/XmlElementTest.php
@@ -35,5 +35,123 @@ class XmlElementTest extends PHPUnit_Framework_TestCase{
         
     }
     
+    /**
+    * Provides an empty object (no arguments to contructor).
+    * 
+    * @return array
+    */
+    public function dataProviderEmptyObject(){
+        
+        return array( 
+                    array( new \Altumo\Xml\XmlElement() )                   
+        );
+        
+    }
+    
+    /**
+    * Test that the object is not loaded when no parameters are passed to the constructor.
+    * 
+    * @dataProvider dataProviderEmptyObject
+    * @param \Altumo\Xml\XmlElement $xml_element
+    */
+    public function testIsNotLoadedOnEmptyContructor( $xml_element ){
+       
+        $this->assertFalse( $xml_element->isLoaded() );
+        
+    }
+    
+    /**
+    * Test that the object throws and exception when assertLoaded is called.
+    * 
+    * @dataProvider dataProviderEmptyObject
+    * @expectedException \Exception
+    * @param \Altumo\Xml\XmlElement $xml_element
+    */
+    public function testAssertLoadedThrowsExceptionOnEmptyContructor( $xml_element ){
+        
+        $xml_element->assertLoaded();
+        
+    }
+    
+    
+    /**
+    * Provides a minimally-size xml file for testing query elements.
+    * 
+    * @array
+    */
+    public function dataProviderMinimalElements(){
+        
+        
+        return array( 
+                    array( new \Altumo\Xml\XmlElement( $this->getTestSuiteFileContents('minimal.xml') ) )                   
+        );
+        
+    }
+    
+    
+    
+    /**
+    * Test the getName method.
+    * 
+    * @dataProvider dataProviderMinimalElements
+    * @param \Altumo\Xml\XmlElement $xml_element
+    */
+    public function testMethodGetName( $xml_element ){
+        
+        $this->assertEquals( 'nutrition', $xml_element->getName() );
+        
+    }
+    
+    
+    /**
+    * Test the queryWithXpath method.
+    * 
+    * @dataProvider dataProviderMinimalElements
+    * @param \Altumo\Xml\XmlElement $xml_element
+    */
+    public function testMethodQueryWithXpath( $xml_element ){
+        
+        $expected = array(
+            "210",
+            "510"
+        );        
+        $this->assertEquals( $expected, $xml_element->queryWithXpath('food/sodium') );
+        
+    }
+    
+    
+    /**
+    * Tests the documention's sample code.
+    * 
+    */
+    public function testDocumentationSampleCode(){
+        
+        $file_contents = file_get_contents( __DIR__ . '/test_data/minimal.xml' );
+        $xml_element = new \Altumo\Xml\XmlElement( $file_contents );
+        $expected = array(
+            "210",
+            "510"
+        );
+        $this->assertEquals( $expected, $xml_element->queryWithXpath('food/sodium', \Altumo\Xml\XmlElement::RETURN_TYPE_STRING) );
+        
+    }
+    
+    
+    
+    
+    /**
+    * Gets the contents of a test suite file for this object as a string.
+    * 
+    * @param string $filename
+    * @return string
+    */
+    protected function getTestSuiteFileContents( $filename ){
+                
+        return file_get_contents( __DIR__ . '/test_data/' . $filename );
+
+    }
+    
 }
+
+
 
