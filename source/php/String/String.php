8e2bf00c75ae32a5a0a1e4a7832050f9814b4d07
Steve Sperandeo <steve.sperandeo@gmail.com>
2011-05-16 15:54:41 -0700

    Added some string helper functions.

--- source/php/String/String.php
+++ source/php/String/String.php
@@ -21,6 +21,7 @@ namespace Altumo\String;
  */
 class String{
 
+    
     /**
     * Inserts one string ($addition) into another ($destination) at a given 
     * string $offset.
@@ -38,6 +39,183 @@ class String{
         $right = substr( $destination, $offset );
         return $left . $addition . $right;
     }
+    
+    
+    /**
+    * Makes an underscored string camel case
+    * eg.
+    *   how_are_you
+    * becomes
+    *   HowAreYou
+    * 
+    * 
+    * @param string $string
+    * @return string
+    */
+    static public function formatCamelCase( $string ) {
+        
+        $output = "";
+        $string_parts = explode( '_', $string );
+        
+        foreach( $string_parts as $string_part ){
+            $string_part = strtolower($string_part);
+            $output .= strtoupper(substr( $string_part, 0, 1 )) . substr( $string_part, 1 ) ;
+        }
+        
+        return $output;
+        
+    }
+    
+    
+    /**
+    * Makes a $string title cased
+    * eg.
+    *   how are you
+    * becomes
+    *   How Are You
+    * 
+    * @see http://blogs.sitepoint.com/title-case-in-php/
+    * 
+    * @param string $string
+    * @return string
+    */
+    static public function formatTitleCase( $string ) {
+        
+        // Converts $string to Title Case, and returns the result.
+        // Our array of 'small words' which shouldn't be capitalised if 
+        // they aren't the first word. Add your own words to taste.
+            $smallwordsarray = array( 'of','a','the','and','an','or','nor','but','is','if','then','else','when', 'at','from','by','on','off','for','in','out','over','to','into','with' ); 
+            
+            // Split the string into separate words 
+            $words = explode(' ', $title); 
+            foreach( $words as $key => $word ){ 
+                // If this word is the first or it's not one of our small words, capitalise it
+                if( $key == 0 || !in_array($word, $smallwordsarray) ){
+                    $words[$key] = ucwords($word);
+                }        
+            } // Join the words back into a string 
+            $newtitle = implode(' ', $words);
+        
+        return $newtitle;
+        
+    }
+    
+    
+    /**
+    * Makes a camel case string underscored 
+    * eg.
+    *   HowAreYou
+    * becomes
+    *   how_are_you
+    * 
+    * 
+    * @param string $string
+    * @return string
+    */
+    static public function formatUnderscored( $string ) {
+        
+        $output = "";
+        //put underscores before the capitals and lower case all characters
+        $output = strtolower( preg_replace('/([A-Z])/', '_\\1', $string) );
+        
+        //remove the first "_", if there is one
+        $output = preg_replace('/^_(.*?)$/m', '\\1', $output);
+        
+        return $output;
+        
+    }
+    
+    
+    /**
+    * Generates a string $number_of_chars long with the $character_pool as potential characters.
+    * 
+    * @param integer $number_of_chars
+    * @param string $number_of_chars
+    */
+    static public function generateRandomString( $number_of_chars, $character_pool = '0123456789abcdefghijklmnopqrstuvwxyz' ){
+        
+        if( !is_integer($number_of_chars) || $number_of_chars < 1 ){
+            throw new \Exception('Number of chars must be a positive integer.');
+        }
+        $output = '';
+        $pool_count = strlen($character_pool);
+        for( $x = 0; $x < $number_of_chars; $x++ ){
+            $index = rand(0,$pool_count-1);
+            $output .= $character_pool[$index];
+        }        
+        return $output;
+        
+    }
+    
+    
+    /**
+    * Generates a url parameter string from the supplied array.
+    * Adds the ? to the beginning.
+    * Returns an empty string if $parameters is empty.
+    * This method will url_encode the values, but not the keys.
+    * 
+    * @param array $parameters
+    * @return string
+    */    
+    static public function generateUrlParameterString( $parameters = array() ){
+        
+        if( empty($parameters) ) return '';
+        
+        //combine and encode the parameters
+            $combined_parameters = array();            
+            foreach( $parameters as $key => $parameter ){
+                $combined_parameters[] = $key . '=' . urlencode($parameter);
+            }
+        
+        //build the request url
+            $parameter_string = '';
+            if( !empty($parameters) ){
+                $parameter_string .= '?' . implode( '&', $combined_parameters );
+            }
+        
+        return $parameter_string;
+        
+    }
+    
+    
+    /**
+    * Formats a number into a human readable string.
+    * 
+    * eg. 1000
+    *   1KB
+    * 
+    * @param integer $bytes
+    * @param integer $precision
+    * @return string
+    */
+    static public function formatBytesToHuman( $bytes, $precision = 2 ){
+        
+        $units = array('B', 'KB', 'MB', 'GB', 'TB');
+      
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+      
+        $bytes /= pow(1024, $pow);
+      
+        return round($bytes, $precision) . ' ' . $units[$pow]; 
+        
+    }
+    
+    
+    /**
+    * Removes all non-alphabetic characters from the given string.
+    * 
+    * @throws \Exception                    //if string is not a string
+    * @param string $string
+    */
+    static public function stripNonAlphaCharacters( $string ){
+        
+        \Altumo\Validation\Strings::assertString($string);
+        return preg_replace('/[^a-zA-Z]/', '', $string);
+        
+    }
+    
     
 }
 
