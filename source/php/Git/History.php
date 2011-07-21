8131358539298bd02e7905c64f019935ca3712ad
Steve Sperandeo <steve.sperandeo@gmail.com>
2011-07-21 11:32:44 -0700

    Moved function within Git/History to sfAltumoPlugin

--- source/php/Git/History.php
+++ source/php/Git/History.php
@@ -20,34 +20,7 @@ namespace Altumo\Git;
 class History{
     
     
-    /**
-    * Gets an array of sql files to move to the appropriate directories.
-    * Files must be in the database/new folder AND be in the current commit to qualify for being moved.
-    * 
-    * @param string $commit_hash
-    * @param string $project_root  //full pathname of the project root (without the trailing slash)
-    * @param string $database_dir  //full pathname of the database directory (without the trailing slash)
-    * @return array
-    */
-    static public function getFilesToMove( $commit_hash, $project_root, $database_dir ){
-        
-          
-        $sql_files_in_filesystem = \Altumo\Utils\Finder::type('file')->name('*.sql')->in( $database_dir . '/new' );
-        
-        $sql_files_in_commit = self::getNewDatabaseFilesByCommit( $commit_hash, $project_root );
-        
-        $move_sql_files = array();
-        foreach( $sql_files_in_filesystem as $sql_file_in_filesystem ){
-            if( in_array($sql_file_in_filesystem, $sql_files_in_commit) ){
-                $move_sql_files[] = $sql_file_in_filesystem;
-            }
-        }
-        
-        return $move_sql_files;
-        
-    }    
-    
-    
+
     /**
     * Gets the last commit as an array.
     * The SHA1 is the array key and the value is a string with the comments of the commit (on one line)
@@ -61,7 +34,7 @@ class History{
             $revisions = self::parseOneLineFormat( $git_log_output );
             return $revisions;
         
-    }    
+    }
     
     
     /**
@@ -76,33 +49,7 @@ class History{
             return reset(array_keys($last_commit));
         
     }    
-    
-    
-    /**
-    * Gets an array of files that were in the supplied commit
-    * 
-    * @param string $commit_hash
-    * @param string $project_root  //full pathname of the project root (without the trailing slash)
-    * 
-    * @return array
-    */
-    static public function getNewDatabaseFilesByCommit( $commit_hash, $project_root ){
-        
-        $git_command = 'git show --name-status ' . $commit_hash;
-        $git_output = `$git_command`;
-           
-        $files = array();
-        preg_match_all( '%^(([DMA])\\s+(database/new/(.*)\\.sql))?$%im', $git_output, $results, PREG_SET_ORDER );     
-        foreach( $results as $result ){
-            if( array_key_exists(3,$result) ){
-                $files[] = $project_root . '/' . $result[3];
-            }            
-        }
-        
-        return $files;
 
-    }
-    
     
     /**
     * Parses the output from git log into an array.
