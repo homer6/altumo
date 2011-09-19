d24943347cad31ea9ebebb82e69e29573e342586
Steve Sperandeo <steve.sperandeo@gmail.com>
2011-09-18 21:05:15 -0700

    Added date validation and string length validation.

--- source/php/Validation/Strings.php
+++ source/php/Validation/Strings.php
@@ -40,7 +40,7 @@ class Strings{
     */
     static public function assertNonEmptyString( $string, $exception_message = null ){
         
-        if( is_null( $exception_message ) ){
+        if( is_null($exception_message) ){
             $exception_message = 'Value passed is not a non-empty string.';
         }
         
@@ -67,7 +67,7 @@ class Strings{
     */
     static public function assertString( $string, $exception_message = null ){
         
-        if( is_null( $exception_message ) ){
+        if( is_null($exception_message) ){
             $exception_message = 'Value passed is not a string.';
         }
         
@@ -76,6 +76,58 @@ class Strings{
         }else{
             return $string;
         }
+                
+    }
+    
+    
+    /**
+    * Throws an exception if this is not a string (or castable as one) or is not
+    * with the string length range provided.
+    * Returns the string if it was cast as one.
+    * 
+    * @param mixed $string
+    * @param integer $min_length
+    * @param integer $max_length
+    * @param string $exception_message  
+    *   //custom Exception message to throw. 
+    *     If null, default message will be used.
+    * 
+    * @throws Exception //if argument passed is not a string
+    * @throws Exception //if the string length was not within the provided range
+    * @return string
+    */
+    static public function assertStringAndLength( $string, $min_length = null, $max_length = null, $exception_message = null ){
+        
+        if( !is_string($string) ){
+            if( is_null($exception_message) ){
+                $exception_message = 'Value passed is not a string.';
+            }
+            throw new \Exception( $exception_message );
+        }
+        
+        $length = strlen( $string );
+        
+        if( !is_null($min_length) ){
+            $min_length = \Altumo\Validation\Numerics::assertUnsignedInteger( $min_length );
+            if( $length < $min_length ){
+                if( is_null($exception_message) ){
+                    $exception_message = 'String length was less than the minimum allowed length ( ' . $min_length . ' ).';
+                }
+                throw new \Exception( $exception_message );
+            }
+        }
+        
+        if( !is_null($max_length) ){
+            $max_length = \Altumo\Validation\Numerics::assertUnsignedInteger( $max_length );
+            if( $length > $max_length ){
+                if( is_null($exception_message) ){
+                    $exception_message = 'String length was greater than the maximum allowed length ( ' . $max_length . ' ).';
+                }
+                throw new \Exception( $exception_message );
+            }
+        }
+        
+        return $string;
                 
     }
 
