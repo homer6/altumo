b0ae007a34214b1b31e948528fb26f54de5f68a0
Steve Sperandeo <steve.sperandeo@gmail.com>
2011-12-05 11:50:28 -0800

    Added time zone convertion to Date.

--- source/php/Utils/Date.php
+++ source/php/Utils/Date.php
@@ -26,6 +26,20 @@ namespace Altumo\Utils;
 class Date extends sfDate\sfDate{
     
     protected $time_zone = null;
+    protected $date_time_zone = null;
+    
+    
+    /**
+    * Constructor for this date object.
+    * 
+    * @param timestamp/string/sfDate $date
+    */
+    public function __construct( $date = null ){
+        
+        parent::__construct( $date );        
+        $this->date_time_zone = new \DateTimeZone( date_default_timezone_get() );        
+        
+    }
     
     
     /**
@@ -36,6 +50,11 @@ class Date extends sfDate\sfDate{
     public function setTimeZone( $time_zone ){
         
         $this->time_zone = $time_zone;
+        try{
+            $this->date_time_zone = new \DateTimeZone( $time_zone );
+        }catch( \Exception $e ){
+                        
+        }
         
     }
 
@@ -51,14 +70,44 @@ class Date extends sfDate\sfDate{
         
     }
     
-    /*
-    public function __set_state( $array ){
-        echo (object) $array;
-    }
-    */
-        
-      // 'time_zone' => NULL,\n   'ts' => 1316456162,\n   'init' => 1316456162,\n))
     
+    /**
+    * Converts this datetime to the supplied timezone.
+    * Uses the default_date_timezone if this Date's timezone hasn't been set.
+    * Returns a new \Altumo\Utils\Date object.
+    * 
+    * @param string|\DateTimeZone $timezone
+    * @return \Altumo\Utils\Date
+    */
+    public function convertToTimeZone( $timezone ){
         
+        if( is_null($this->date_time_zone) ){
+            $current_timezone = new \DateTimeZone( date_default_timezone_get() );
+        }else{
+            $current_timezone = $this->date_time_zone;
+        }
+        
+        if( !($timezone instanceof \DateTimeZone) ){
+            $user_timezone = new \DateTimeZone( $timezone );
+        }else{
+            $user_timezone = $timezone;
+        }
+        
+        if( $current_timezone->getName() == $user_timezone->getName() ){
+            return new \Altumo\Utils\Date( $this->format('U') );
+        }
+        
+        $system_timezone = $current_timezone;
+        $user_time = new \DateTime( $this->format('c') );
+        $system_offset = $system_timezone->getOffset( $user_time );
+        $user_offset = $user_timezone->getOffset( $user_time );
+        
+        $new_date = new \Altumo\Utils\Date( $user_time->format('U') + ( -1 * $system_offset ) +  $user_offset );
+        $new_date->setTimeZone( $user_timezone->getName() );
+        
+        return $new_date;
+        
+    }
+
     
 }
